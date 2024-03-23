b2283b6bffefe1766ae954de7cbee986816c6c32
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-03-23 16:01:22 +0200

    update the header UI

--- projects/admin/src/app/core/core.module.ts
+++ projects/admin/src/app/core/core.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { headerComponent } from './components/header/header.component';
-
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [headerComponent],
+  imports: [MatIconModule],
   exports: [headerComponent],
 })
 export class coreModule {}
