3eab81e09d1f817e369ad2634638c5fcfee6940d
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-04-07 08:30:03 +0200

    create the tabe as shared component

--- projects/admin/src/app/shared/shared.module.ts
+++ projects/admin/src/app/shared/shared.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { materialModule } from './modules/materila.module';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  imports: [materialModule],
-  exports: [materialModule, CommonModule, ReactiveFormsModule],
+  imports: [materialModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  exports: [
+    materialModule,
+    CommonModule,
+    ReactiveFormsModule,
+    DataTableComponent,
+  ],
+  declarations: [DataTableComponent],
 })
 export class sharedModule {}
