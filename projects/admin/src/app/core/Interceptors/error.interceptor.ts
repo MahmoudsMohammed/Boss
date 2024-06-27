717cb1ba520007d770b8988e50457416d944c497
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-06-27 08:16:50 +0300

    navigate to login with jwt errors

--- projects/admin/src/app/core/Interceptors/error.interceptor.ts
+++ projects/admin/src/app/core/Interceptors/error.interceptor.ts
@@ -5,18 +5,23 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable()
 export class errorInterceptor implements HttpInterceptor {
   toaster = inject(ToastrService);
+  router = inject(Router);
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((err) => {
+        if (err.error.message.includes('jwt')) {
+          this.router.navigate(['/login']);
+        }
         this.toaster.error(err.error.message);
         return throwError(err);
       })
