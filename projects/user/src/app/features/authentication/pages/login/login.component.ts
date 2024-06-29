0d938df3b9f00e934893c4cd8652cdba0cd5c685
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-06-29 07:21:11 +0300

    setup the user login form

--- projects/user/src/app/features/authentication/pages/login/login.component.ts
+++ projects/user/src/app/features/authentication/pages/login/login.component.ts
@@ -1,14 +1,36 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  hide = signal(true);
+  passwordError = signal<string>('');
+  emailError = signal<string>('');
+  userLoginForm: FormGroup;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userLoginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
 }
